e6add2cec2be6591a28598eac16a8c05d4ca72bd
Romuald Brillout <git@brillout.com>
2025-04-20 23:56:11 +0200

    simplify

--- test.js
+++ test.js
@@ -4,25 +4,11 @@ import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const vikeReactDir = path.join(__dirname, "packages/vike-react/");
+const uselessCwd = '/'
 
 try {
   logSep()
-  console.log(`createRequire('${import.meta.url}')`, {
-    paths: vikeReactDir,
-  });
-  const require_ = createRequire(import.meta.url);
-  const resolved = require_.resolve("vike-react/feature", {
-    paths: [vikeReactDir],
-  });
-
-  console.log(resolved);
-} catch (error) {
-  console.log(error);
-}
-
-try {
-  logSep()
-  console.log(`createRequire('${__dirname}')`, {
+  console.log(`createRequire('${uselessCwd}')`, {
     paths: vikeReactDir,
   });
   const require_ = createRequire(__dirname);
@@ -38,23 +24,12 @@ try {
 try {
   logSep()
   console.log(`createRequire('${vikeReactDir}')`, {
-    paths: __dirname,
+    paths: uselessCwd,
   });
   const require_ = createRequire(vikeReactDir);
   const resolved = require_.resolve("vike-react/feature", {
-    paths: [__dirname],
+    paths: [uselessCwd],
   });
-
-  console.log(resolved);
-} catch (error) {
-  console.log(error);
-}
-
-try {
-  logSep()
-  console.log(`createRequire('${vikeReactDir}')`);
-  const require_ = createRequire(vikeReactDir);
-  const resolved = require_.resolve("vike-react/feature");
 
   console.log(resolved);
 } catch (error) {
